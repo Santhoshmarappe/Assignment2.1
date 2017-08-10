9c963a2c245d3b1df0c107d061c35f43e95a2fa8
Santhoshmarappe <santhoshkumar.marappe@epsilon.com>
2017-08-10 12:16:51 +0530

    Assignment2.1 arrow function

---
+++ function.js
@@ -0,0 +1,6 @@
+//Fat Arrow or Lambda Function
+var favCar = function (CarName, VehNo) {
+    document.write("My fav car is " + CarName + " and vehical no is " + VehNo);
+};
+favCar("Audi", 56789);
+//Output:"My fav car is Audi and vehical no is 56789"SS 
---
+++ function.ts
@@ -0,0 +1,9 @@
+//Fat Arrow or Lambda Function
+
+let favCar = (CarName: string, VehNo:number) => {
+document.write(`My fav car is ${CarName} and vehical no is ${VehNo}`) 
+}
+
+favCar(`Audi`, 56789);
+
+//Output:"My fav car is Audi and vehical no is 56789"SS
